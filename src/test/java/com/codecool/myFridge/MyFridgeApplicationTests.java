7d730bf045f2a047d90deb14e9bb0d8ab45c0644
Tihamer Antal <118159451+Antal01@users.noreply.github.com>
2024-01-31 15:31:36 +0100

    Start

---
+++ src/test/java/com/codecool/myFridge/MyFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.myFridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
